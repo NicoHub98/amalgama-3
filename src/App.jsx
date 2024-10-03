56ca6eb837c23cdeeb82e76ebc27e6bed375a4bf
NicoHub98 <nicolud98@gmail.com>
2024-10-03 16:43:43 -0300

    initial config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center w-full h-screen">
+        Amalgama
+      </h1>
+    </>
+  );
+}
+
+export default App;
